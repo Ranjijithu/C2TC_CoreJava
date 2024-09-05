931885135cc7ab322567da89885d3681bafa1eda
ELCOT <ELCOT@Lenovo>
2024-09-05 22:25:31 +0530

    this is my third commit

---
+++ C2TC_siva/src/com/tns/daytwo/brreakdemo.java
@@ -0,0 +1,17 @@
+package com.tns.daytwo;
+
+public class brreakdemo {
+
+	public static void main(String[] args) {
+		for (int i = 5; i < 10; i++) {
+			if (i == 5)
+			break;
+			System.out.println(i);
+			}
+			}
+			
+		// TODO Auto-generated method stub
+
+	}
+
+
---
+++ C2TC_siva/src/com/tns/daytwo/whiledemoo.java
@@ -0,0 +1,19 @@
+package com.tns.daytwo;
+
+public class whiledemoo {
+
+	public static void main(String[] args) {
+		{
+			int i=99;
+			while(i<=100)
+			{
+			System.out.println(i);
+			i++;
+			}
+			}
+			}
+		// TODO Auto-generated method stub
+
+	}
+
+
