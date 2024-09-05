bf43a14e9a61acdecaffdcc2026511aad58a5b40
ELCOT <ELCOT@Lenovo>
2024-09-05 23:33:24 +0530

    this is my encapsulationdemo

---
+++ C2TC_siva/src/com/tns/encapsulation/encapsulationdemo.java
@@ -0,0 +1,16 @@
+package com.tns.encapsulation;
+
+public class encapsulationdemo {
+	//object creation
+	OopsConceptDemo obj = new
+
+	OopsConceptDemo();
+
+	obj.setSerialNum(101);
+	obj.setName("Pooja");
+	obj.setAge(20);
+	System.out.println(obj);
+	
+	}	
+
+
